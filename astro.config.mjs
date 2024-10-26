9b67ae384df2f27efedd3f739c4b73002c053a28
sumaolin <sumaolin619@qq.com>
2024-10-26 15:25:30 +0800

    feat(netlify.toml): netlify 服务器部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sumaolin.com",
-  integrations: [react()]
+  site: "https://sumaolin.netlify.app",
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
+  integrations: [react()],
 });
